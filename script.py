2aed8282d83bb466a34c782011f438349c1951e8
gabrielmsidik <gabriel.m.sidik@gmail.com>
2018-12-23 21:45:27 +0800

    updated script to allow for better error handling

--- script.py
+++ script.py
@@ -1,8 +1,14 @@
 import requests
 import sys
-import urllib3
 from bs4 import BeautifulSoup
 
+file_extensions_to_download = [
+    'pdf',
+    'pptx',
+    'ppt',
+    'docx',
+    'doc'
+]
 def begin_scraping(root_link, target_download_folder):
     request = requests.get(root_link)
 
@@ -12,9 +18,13 @@ def begin_scraping(root_link, target_download_folder):
     for link_element in html_content.select('a'):
         link = link_element['href']
         link_components = link.split('.')
-        if (link_components[-1] == 'pdf' or link_components[-1] == 'pptx'):
-            document_name = link_components[-2].split('/')[-1]
-            download_file(link, document_name + '.' + link_components[-1], target_download_folder)
+        if (link_components[-1] in file_extensions_to_download):
+            print(link)
+            try: 
+                document_name = link_components[-2].split('/')[-1]
+                download_file(link, document_name + '.' + link_components[-1], target_download_folder)
+            except Exception:
+                print("WARNING: Error occured from file download")
 
 
 def download_file(download_url, fileName, download_folder_location):
@@ -36,14 +46,17 @@ def main():
     2. TARGET DOWNLOAD FOLDER (OPTIONAL PARAMETER)
     '''
 
-    if len(sys.argv) == 1:
-        print('Please pass the URL of the webpage to scrape as a parameter')
-    elif len(sys.argv) == 2:
+    target_download_folder = './downloads/'
+
+    try:
         root_link = sys.argv[1]
-        target_download_folder = './downloads/'
+    except IndexError:
+        print('ERROR: Please pass target webpage URL')
+        exit()
+
+    if len(sys.argv) == 2:
         begin_scraping(root_link, target_download_folder)
     else:
-        root_link = sys.argv[1]
         target_download_folder = sys.argv[2]
         begin_scraping(root_link, target_download_folder)
 
